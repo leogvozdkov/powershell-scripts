c416efd36590c2cb623a332963ddfb60fc2c01c4
Leo Gvozdkov <leo.gvozdkov@gmail.com>
2020-03-03 13:27:37 +0300

    new file:   AD-GroupCleaner.ps1

---
+++ AD-GroupCleaner.ps1
@@ -0,0 +1,11 @@
+﻿# delete all member of AD group, add admins
+#
+# e.g. to close shares from users and let admins control them
+#
+
+import-module activedirectory
+
+$group = Read-Host 'group?'
+
+Get-ADGroupMember $group | ForEach-Object {Remove-ADGroupMember $group $_ -Confirm:$false}
+Add-ADGroupMember $group administrator      # possible to add any AD user
